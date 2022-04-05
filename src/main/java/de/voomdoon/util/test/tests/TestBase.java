2ef4a82c5783c11f69c8dbee8fb4c73171b1d36e
voomdoon <nilreb24-github20141105@yahoo.de>
2022-04-05 22:22:43 +0200

    feat: add TestBase
    [DEV-151]

---
+++ src/main/java/de/voomdoon/util/test/tests/TestBase.java
@@ -0,0 +1,122 @@
+package de.voomdoon.util.test.tests;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+
+import de.voomdoon.logging.LogManager;
+import de.voomdoon.logging.Logger;
+
+/**
+ * Base class for tests.
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+public abstract class TestBase {
+
+	/**
+	 * @since 0.1.0
+	 */
+	private static final String TEMP_DIRECTORY_NAME = "target/temp";
+
+	/**
+	 * @since 0.1.0
+	 */
+	protected final Logger logger = LogManager.getLogger(getClass());
+
+	/**
+	 * @since 0.1.0
+	 */
+	private Path tempDirectory;
+
+	/**
+	 * @since 0.1.0
+	 */
+	private String testMethodName;
+
+	/**
+	 * @return {@link Path}
+	 * @since 0.1.0
+	 */
+	protected synchronized Path getTempDirectory() throws IOException {
+		tempDirectory = Paths.get(TEMP_DIRECTORY_NAME);
+
+		if (!Files.exists(tempDirectory)//
+				|| Files.isRegularFile(tempDirectory)// get proper exception
+		) {
+			Files.createDirectory(tempDirectory);
+		}
+
+		return Paths.get(TEMP_DIRECTORY_NAME);
+	}
+
+	/**
+	 * @return {@link String}
+	 * @since 0.1.0
+	 */
+	protected String getTestMethodName() {
+		if (testMethodName == null) {
+			throw new IllegalStateException("Test method name not known => 'logTestStart' not called!");
+		}
+
+		return testMethodName;
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	@AfterEach
+	protected void logTestEnd() {
+		logger.debug("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	protected void logTestStart() {
+		testMethodName = determineTestMethodName();
+
+		logger.debug("+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +");
+		logger.info("running test '" + testMethodName + "'...");
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	@AfterEach
+	void tearDownTempDirectory() {
+		if (tempDirectory != null && Files.exists(tempDirectory)) {
+			try {
+				FileUtils.deleteDirectory(tempDirectory.toFile());
+			} catch (IOException e) {
+				// TODO implement error handling
+				throw new RuntimeException("Error at 'tearDownTempDirectory': " + e.getMessage(), e);
+			}
+		}
+	}
+
+	/**
+	 * @return {@link String}
+	 * @since 0.1.0
+	 */
+	private String determineTestMethodName() {
+		int i = 3;
+		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+		while (true) {
+			String name = stackTrace[i].getMethodName();
+
+			if (!name.equals("logTestStart")) {
+				return name;
+			}
+
+			i++;
+		}
+	}
+}
---
+++ src/test/java/de/voomdoon/util/test/tests/TestBaseTest.java
@@ -0,0 +1,217 @@
+package de.voomdoon.util.test.tests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for {@link TestBase}.
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+public abstract class TestBaseTest {
+
+	/**
+	 * Test class for {@link TestBase#getTempDirectory()}.
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	static class GetTempDirectoryTest extends TestBase {
+
+		/**
+		 * @since 0.1.0
+		 */
+		private static final String TARGET_TEMP = "target/temp";
+
+		/**
+		 * @throws IOException
+		 * @since 0.1.0
+		 */
+		@Test
+		void test() throws IOException {
+			logTestStart();
+
+			Path actual = getTempDirectory();
+
+			assertThat(actual).exists().isDirectory();
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_before() {
+			logTestStart();
+
+			Path actual = Paths.get(TARGET_TEMP);
+
+			assertThat(actual).doesNotExist();
+		}
+
+		@Test
+		void test_exists_file() throws Exception {
+			logTestStart();
+
+			Path file = Paths.get(TARGET_TEMP);
+			Files.createFile(file);
+
+			try {
+				assertThrows(FileAlreadyExistsException.class, () -> getTempDirectory());
+			} finally {
+				Files.delete(file);
+			}
+		}
+
+		/**
+		 * @throws IOException
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_twice() throws IOException {
+			logTestStart();
+
+			assertThat(getTempDirectory()).isEqualTo(getTempDirectory());
+		}
+	}
+
+	/**
+	 * Test class for {@link TestBase#getTestMethodName()}.
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	static class GetTestMethodNameTest extends TestBase {
+
+		@Test
+		void test_error_IllegalStateException() throws Exception {
+			logTestStart();
+
+			MyTestBase test = new MyTestBase();
+
+			assertThrows(IllegalStateException.class, () -> test.getTestMethodName());
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_overwriteLogTestStart() {
+			logTestStart();
+
+			MyTestBase test = new MyTestBase();
+			test.logTestStart();
+
+			assertThat(test.getTestMethodName()).isEqualTo("test_overwriteLogTestStart");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_someName() {
+			logTestStart();
+
+			assertThat(getTestMethodName()).isEqualTo("test_someName");
+		}
+	}
+
+	/**
+	 * Test class for {@link TestBase#logTestStart()}.
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	static class LogTestStartTest extends TestBase {
+
+		/**
+		 * @throws Exception
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_someName() throws Exception {
+			logTestStart();
+
+			new MyTestBase().logTestStart();
+
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			System.setOut(new PrintStream(out));
+
+			new MyTestBase().logTestStart();
+
+			assertThat(new String(out.toByteArray())).contains("test_someName");
+		}
+	}
+
+	/**
+	 * Test class for {@link TestBase#te}.
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	static class TearDowntempDirectoryTest extends TestBase {
+
+		/**
+		 * @throws Exception
+		 * @since 0.1.0
+		 */
+		@Test
+		void test() throws Exception {
+			logTestStart();
+
+			Path actual = getTempDirectory();
+
+			tearDownTempDirectory();
+
+			assertThat(actual).doesNotExist();
+		}
+
+		@Test
+		void test_inUse() throws Exception {
+			logTestStart();
+
+			Path temp = getTempDirectory();
+
+			BufferedWriter bw = new BufferedWriter(new FileWriter(temp + "/file"));
+
+			assertThrows(RuntimeException.class, () -> tearDownTempDirectory());
+
+			assertThat(temp).exists().isDirectory();
+
+			bw.close();
+		}
+	}
+
+	/**
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	private static class MyTestBase extends TestBase {
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		protected void logTestStart() {
+			super.logTestStart();
+		}
+	}
+}
